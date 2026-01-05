d23939cd01dc1c160e69686eed4175010099beab
Andry <andrysitrakiniainasoa@gmail.com>
2026-01-05 16:07:51 +0300

    rereun training

--- src/sentiment_analysis/utils.py
+++ src/sentiment_analysis/utils.py
@@ -11,16 +11,18 @@ def _get_absolute_path(relative_path):
         str: The absolute file path.
     """
 
-    current_directory = os.getcwd()
+    current_directory = os.path.dirname(os.path.abspath(__file__))
     joined_path = os.path.join(current_directory, relative_path)
     absolute_path = os.path.abspath(joined_path)
 
     return absolute_path
 
 
-PATH_TRAINING_DATASET = _get_absolute_path('../data/raw/IMDB Dataset.csv')
+PATH_TRAINING_DATASET = _get_absolute_path('../../data/raw/IMDB Dataset.csv')
 
-PATH_CLEANED_TRAINING_DATASET = _get_absolute_path('../data/processed/cleaned-dataset-training.csv')
+PATH_CLEANED_TRAINING_DATASET = _get_absolute_path('../../data/processed/cleaned-dataset-training.csv')
 
-PATH_MODEL = _get_absolute_path('../model/lstm_model.keras')
+PATH_MODEL = _get_absolute_path('../../model/lstm_model.keras')
+
+PATH_GLOVE_EMBEDDINGS = _get_absolute_path('../../data/raw/glove.6B.100d.txt')
 
